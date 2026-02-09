c785aa8d5c0e53f0e7213d008387f0424a68d09d
Jyothish <jyothishmasarpu@gmail.com>
2026-02-09 07:39:18 +0530

    Added PalindromeTriangle pattern

---
+++ StarPatterns/PalindromeTriangle.java
@@ -0,0 +1,21 @@
+package StarPatterns;
+public class PalindromeTriangle
+{
+	public static void main(String[] args) {
+	    int n=6;
+		int i,j;
+        System.out.println("Palindrome Triangle:");
+		for(i=1;i<=n;i++){
+		    for(j=1;j<=(n-i);j++){
+		        System.out.print("  ");
+		    }
+		    for(j=i;j>=1;j--){
+		        System.out.print(j+" ");
+		    }
+		    for(j=2;j<=i;j++){
+		        System.out.print(j+" ");
+		    }
+		    System.out.println();
+		}
+	}
+}
