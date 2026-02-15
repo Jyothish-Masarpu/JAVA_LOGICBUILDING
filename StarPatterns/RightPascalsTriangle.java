8838c74be5674a019cd0c5f9a2e76e7b9b299c95
Jyothish <jyothishmasarpu@gmail.com>
2026-02-15 08:58:18 +0530

    Added Star patterns

---
+++ StarPatterns/RightPascalsTriangle.java
@@ -0,0 +1,19 @@
+package StarPatterns;
+public class RightPascalsTriangle {
+    public static void main(String[] args) {
+        int n=4;
+        System.out.println("Right Pascals Triangle:");
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
